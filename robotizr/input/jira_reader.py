b2e3fc72ca93695a059cfc8cdbeae9b7405f453a
Sven Kiera <sven@kiera.de>
2019-11-19 16:17:19 +0100

    #1 Fixed missing props

--- robotizr/input/jira_reader.py
+++ robotizr/input/jira_reader.py
@@ -1,3 +1,7 @@
+import shutil
+import base64
+import urllib.request
+
 from jira import JIRA
 from robotizr.core import template
 from robotizr.data.test_case import TestCase, Keyword
@@ -7,35 +11,44 @@ from robotizr.data.test_suite import TestSuite, Settings
 class JiraReader(object):
 
     def __init__(self, config):
-        self.config = config
-        self.jira = JIRA(config['server'],
-                         auth=(config['username'], config['password']))
-        self.cache = {}
+        self._config = config
+        self._jira = JIRA(config['server'],
+                          auth=(config['username'], config['password']))
+        self._cache = {}
 
     def get_test(self, issue_id):
-        if issue_id not in self.cache:
-            self.cache[issue_id] = self.jira.issue(issue_id)
-        return self.cache[issue_id]
+        if issue_id not in self._cache:
+            self._cache[issue_id] = self._jira.issue(issue_id)
+        return self._cache[issue_id]
 
-    def convert_tests(self, query):
-        issues = self.jira.search_issues(query)
+    def convert_tests(self, query, props):
+        issues = self._jira.search_issues(query)
         suites = {}
         for issue in issues:
             suite_name = template.get_string_value_for_placeholder(self, issue,
-                                                                   self.config['mappings']['test_suite']['name'])
+                                                                   self._config['mappings']['test_suite']['name'], props)
             if suite_name not in suites:
-                suites[suite_name] = self._convert_obj(self.config["mappings"]["test_suite"], issue, TestSuite)
+                suites[suite_name] = self._convert_obj(self._config["mappings"]["test_suite"], issue, props, TestSuite)
             suite = suites[suite_name]
-            suite.test_cases.append(self._convert_obj(self.config["mappings"]["test_case"], issue, TestCase))
+            suite.test_cases.append(self._convert_obj(self._config["mappings"]["test_case"], issue, props, TestCase))
         return list(suites.values())
 
-    def _convert_obj(self, mappings, issue, clazz):
+    def save_attachment(self, url, file_name):
+        credentials = "%s:%s" % (self._config['username'], self._config['password'])
+        headers = {"Authorization": "Basic %s" % str(base64.b64encode(credentials.encode("utf-8")), "utf-8")}
+
+        request = urllib.request.Request(url, headers=headers)
+        with urllib.request.urlopen(request) as response:
+            with open(file_name, "wb") as out_file:
+                shutil.copyfileobj(response, out_file)
+
+    def _convert_obj(self, mappings, issue, props, clazz):
         obj = clazz()
         for field in mappings:
             if clazz is TestCase and field == "keywords":
-                template.evaluate(self, obj, issue, field, mappings[field], Keyword)
+                template.evaluate(self, obj, issue, field, mappings[field], props, Keyword)
             elif clazz is TestSuite and field == "settings":
-                template.evaluate(self, obj, issue, field, mappings[field], Settings)
+                template.evaluate(self, obj, issue, field, mappings[field], props, Settings)
             else:
-                template.evaluate(self, obj, issue, field, mappings[field])
+                template.evaluate(self, obj, issue, field, mappings[field], props)
         return obj
